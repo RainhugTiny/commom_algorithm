a3fad70d9b1245cb904dab690f6130208866b340
wangtianrun <m17888819538@163.com>
2020-02-23 20:21:50 +0800

    quick sort

---
+++ quick_sort_v1.py
@@ -0,0 +1,16 @@
+def quick_sort(array, left, right):
+    if left < right:  # left right 为索引
+        location = partition(array, left, right)
+        quick_sort(array, left, location - 1)
+        quick_sort(array, location + 1, right)
+
+
+def partition(array, left, right):
+    target = array[right]
+    i = left - 1  # i代表目标值左侧(比target小的值)有序部分的末尾的索引
+    for j in range(left, right):  # j代表目标值右侧（比target值大的有序部分末尾的索引）
+        if array[j] < target:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[right] = array[right], array[i + 1]
+    return i + 1
