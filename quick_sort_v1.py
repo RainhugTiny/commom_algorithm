9074babf2d99cf950aeebe3e295076db886eaefc
wangtianrun <m17888819538@163.com>
2020-03-03 09:12:30 +0800

    fix bug

--- quick_sort_v1.py
+++ quick_sort_v1.py
@@ -7,10 +7,10 @@ def quick_sort(array, left, right):
 
 def partition(array, left, right):
     target = array[right]
-    i = left - 1  # i代表目标值左侧(比target小的值)有序部分的末尾的索引
-    for j in range(left, right):  # j代表目标值右侧（比target值大的有序部分末尾的索引）
+    i = left - 1  # i代表目标值左侧(比target小的值)部分的末尾的索引
+    for j in range(left, right):  # j代表目标值右侧（比target值大的部分末尾的索引）
         if array[j] < target:
             i += 1
             array[i], array[j] = array[j], array[i]
     array[i + 1], array[right] = array[right], array[i + 1]
-    return i + 1
+    return i + 1  # 返回分界值的位置
